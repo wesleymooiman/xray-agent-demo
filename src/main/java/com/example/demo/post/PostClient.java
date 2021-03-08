4e1c914aa49003a2f80f42c969df25e335bb55c0
Wesley Mooiman <wesley@craftsmen.nl>
2021-03-08 10:17:17 +0100

    small refactor

--- src/main/java/com/example/demo/post/PostClient.java
+++ src/main/java/com/example/demo/post/PostClient.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(url = "https://jsonplaceholder.typicode.com", name = "post")
 public interface PostClient {
 
     @GetMapping("/posts")
     List<Post> getPosts();
+
+    @GetMapping("/posts/{id}")
+    Post getPost(@PathVariable("id") final int id);
 
 }
 
--- src/main/java/com/example/demo/post/PostController.java
+++ src/main/java/com/example/demo/post/PostController.java
@@ -1,13 +1,16 @@
 package com.example.demo.post;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,29 +19,39 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/posts")
+@AllArgsConstructor
 public class PostController {
+
+    private static final int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
+    private static final ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(AVAILABLE_PROCESSORS);
 
     private final PostClient postClient;
 
-    @Autowired
-    public PostController(PostClient postClient) {
-        this.postClient = postClient;
-    }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public List<List<Post>> all() {
-        final List<Future<List<Post>>> futureList = IntStream.range(1, 10)
-                .mapToObj(i -> CompletableFuture.supplyAsync(postClient::getPosts))
+    public List<Post> all() {
+        final List<CompletableFuture<Post>> completableFutures = IntStream.range(1, 20)
+                .mapToObj(this::getPost)
                 .collect(Collectors.toList());
 
-        return futureList.stream().map(this::getResultFromFeature).collect(Collectors.toList());
+        final CompletableFuture<List<Post>> allCompletableFuture = CompletableFuture
+                .allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size()]))
+                .thenApply(future -> completableFutures
+                        .stream()
+                        .map(CompletableFuture::join)
+                        .collect(Collectors.toList())
+                );
+
+        try {
+            return allCompletableFuture.thenApply(greets -> greets.stream()
+                    .filter(Objects::nonNull).collect(Collectors.toList())).get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    private List<Post> getResultFromFeature(Future<List<Post>> future) {
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            return List.of();
-        }
+    private CompletableFuture<Post> getPost(int id) {
+        return CompletableFuture.supplyAsync(() -> postClient.getPost(id), scheduledThreadPool).exceptionally(ex -> null);
     }
 }
